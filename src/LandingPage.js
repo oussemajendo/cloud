53424b270835b1440003eeae3c33c40dff589dcf
oussama jendoubi <oussemajendo@gmail.com>
2023-05-27 11:53:24 +0100

    first commit

---
+++ src/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Map from './Map';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Landing Page</h1>
+      <Map />
+    </div>
+  );
+};
+
+export default LandingPage;
